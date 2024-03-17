7a6bb058b749a77e9ad1ea0a97168c4cf7a40437
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-03-17 18:11:13 +0100

    better formatting of confidence

--- wttr_notifier.py
+++ wttr_notifier.py
@@ -91,7 +91,7 @@ def main(
                 newline = f"Raining {DAY_PARSER[iday]} "
                 newline += ", ".join(
                     [
-                        f"{TIME_PARSER[ir]} ({depth[iday][ir]}mm {confidence[iday][ir]})"
+                        f"{TIME_PARSER[ir]} ({depth[iday][ir]}mm {confidence[iday][ir]:02d}%)"
                         for ir, r in enumerate(raining[iday])
                         if r
                     ]
