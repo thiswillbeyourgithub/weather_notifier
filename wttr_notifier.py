13ae4c436b9b7d274a2c850a6083fa49f575be6c
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-03-22 18:17:36 +0100

    fix: number formatting

--- wttr_notifier.py
+++ wttr_notifier.py
@@ -153,7 +153,7 @@ def main(
             newline = f"Raining {DAY_PARSER[iday]} "
             newline += ", ".join(
                 [
-                    f"{TIME_PARSER[ir]} ({depth[iday][ir]}mm {int(confidence[iday][ir]):03d}%)"
+                    f"{TIME_PARSER[ir]} ({depth[iday][ir]}mm {int(confidence[iday][ir]):02d}%)"
                     for ir, r in enumerate(raining[iday])
                     if r
                 ]
